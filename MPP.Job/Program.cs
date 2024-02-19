0d1e2130f6d755f1d0506d9e58e5818d52c3870f
Danny Wahyudi <wahyudidanny23@gmail.com>
2024-02-19 11:00:09 +0700

    1. deploy

--- MPP.Job/Program.cs
+++ MPP.Job/Program.cs
@@ -28,7 +28,7 @@ public class Program
 
         appSettings = new AppSettings(configuration);
         settingWAChrome = new SettingWAChrome(configuration);
-        
+
         var services = new ServiceCollection();
         services.RegisterContext(configuration);
         services.RegisterService(configuration);
@@ -48,8 +48,8 @@ public class Program
             {
                 try
                 {
-                    GeneratePDF(data.Company, data.Location, "2024", "1",httpClientFactory);
-                   
+                    GeneratePDF(data.Company, data.Location, "2024", "1", httpClientFactory);
+
                 }
                 catch (Exception err)
                 {
@@ -60,8 +60,9 @@ public class Program
 
         }
 
-        if (appSettings.GetValue("flagSendWA") == "1"){
-              openChromeWhatsap();
+        if (appSettings.GetValue("flagSendWA") == "1")
+        {
+            openChromeWhatsap();
 
 
 
@@ -69,41 +70,41 @@ public class Program
 
 
 
-         
+
 
     }
 
-        public static async Task<RestResponse> GetAsync(string url, string? jwtToken = "")
-        {
-                var client = new RestClient();
-                //client.Timeout = -1;
-                var request = new RestRequest(url, Method.Get);
-                request.AddHeader("Content-Type", "application/json");
-                if (!string.IsNullOrEmpty(jwtToken))
-                    request.AddHeader("Authorization", $"Bearer {jwtToken}");
+    public static async Task<RestResponse> GetAsync(string url, string? jwtToken = "")
+    {
+        var client = new RestClient();
+        //client.Timeout = -1;
+        var request = new RestRequest(url, Method.Get);
+        request.AddHeader("Content-Type", "application/json");
+        if (!string.IsNullOrEmpty(jwtToken))
+            request.AddHeader("Authorization", $"Bearer {jwtToken}");
 
-                return await client.ExecuteAsync(request);
-     
-        }
+        return await client.ExecuteAsync(request);
 
-    private async  static void GeneratePDF(string? company, string? location, string tahun, string bulan, IHttpClientFactory httpClientFactory)
+    }
+
+    private async static void GeneratePDF(string? company, string? location, string tahun, string bulan, IHttpClientFactory httpClientFactory)
     {
 
         string apiUrl = $"{settingWAChrome.apiBaseUrl}/api/MPP/GenerateApprovalMPP/?company={company}&location={location}&tahun={tahun}&bulan={bulan}";
 
         using (var httpClient = httpClientFactory.CreateClient())
         {
-              var response = await httpClient.GetAsync(apiUrl);
+            var response = await httpClient.GetAsync(apiUrl);
 
-          if (response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
             {
-                    string responseBody = await response.Content.ReadAsStringAsync();
+                string responseBody = await response.Content.ReadAsStringAsync();
 
             }
         }
 
 
-             
+
 
 
     }
@@ -112,34 +113,32 @@ public class Program
     private static async Task openChromeWhatsap()
     {
 
-        try
+
+        string ChromeUserData = settingWAChrome.chromeUserData;
+
+        var options = new ChromeOptions();
+        options.AddArgument(ChromeUserData);
+
+
+        using (IWebDriver driver = new ChromeDriver(settingWAChrome.chromeDriverPath, options))
         {
-
-            string ChromeUserData = settingWAChrome.chromeUserData;
-
-            var options = new ChromeOptions();
-            options.AddArgument(ChromeUserData);
-
-
-            using (IWebDriver driver = new ChromeDriver(settingWAChrome.chromeDriverPath, options))
+            try
             {
-
                 driver.Navigate().GoToUrl("https://web.whatsapp.com/");
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMinutes(60);
-                Thread.Sleep(10000);
+                Thread.Sleep(3000);
                 SendToWA(driver);
-
                 driver.Quit();
+
             }
+            catch (Exception ex)
+            {
+                driver.Quit();
+                Console.WriteLine(ex.ToString());
 
-
+            }
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
 
-
-        }
     }
 
 
@@ -150,19 +149,17 @@ public class Program
         IWebElement searchBox = driver.FindElement(By.XPath(settingWAChrome.waSearchBox));
         searchBox.SendKeys(settingWAChrome.waRecipientGroup);
 
-        Thread.Sleep(3000); 
- 
+        Thread.Sleep(3000);
+
         driver.FindElement(By.XPath(string.Format(settingWAChrome.waSelectSearchResult ?? "", "VRA"))).Click();
 
-        IWebElement attachmentButton = driver.FindElement(By.CssSelector(settingWAChrome.waAttachmentButton));
-        attachmentButton.Click();
-        Thread.Sleep(5000); 
-        
-        
         string[] files = Directory.GetFiles(appSettings.GetValue("filePathRiau"));
 
         foreach (string file in files)
         {
+            IWebElement attachmentButton = driver.FindElement(By.CssSelector(settingWAChrome.waAttachmentButton));
+            attachmentButton.Click();
+            Thread.Sleep(5000);
             IWebElement fileInput = driver.FindElement(By.CssSelector(settingWAChrome.waInputFileDefault));
             fileInput.SendKeys(file);
             driver.FindElement(By.XPath(settingWAChrome.waTextBoxMessageImage)).SendKeys(Keys.Enter);
